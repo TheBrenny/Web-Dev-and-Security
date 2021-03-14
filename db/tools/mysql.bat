e3e9464eb4b0673244d7e9c15ca544784c4b54c4
TheBrenny <iam+dev@justbrenny.me>
2021-03-15 08:12:03 +1100

    fixed mysql start script

--- db/tools/mysql.bat
+++ db/tools/mysql.bat
@@ -1,2 +1,2 @@
 docker start wdsmysql || docker run --name wdsmysql --ip 127.10.1.1 -p 3306:3306 -v "%cd%/mysql":/var/lib/mysql -e MYSQL_USER=user -e MYSQL_PASSWORD=pass -e MYSQL_DATABASE=wds -e MYSQL_ROOT_PASSWORD=pass -d mysql
-mysqlcli.bat
+call "%~dp0mysqlcli.bat"
