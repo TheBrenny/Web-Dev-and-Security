9a682549a2b8ebb528eb0c383088f2242cc1ef9e
TheBrenny <iam+dev@justbrenny.me>
2021-03-22 10:52:04 +1100

    chains aren't keyrings lol rip.

--- install.js
+++ install.js
@@ -7,31 +7,28 @@ let readScript = (s) => fs.readFileSync(path.join(dbScripts, s)).toString();
 let prom = Promise.resolve();
 
 if (process.argv.includes("cleanDB")) {
-    prom.then(() => {
-        let sql = readScript("clean.sql");
-        console.log(sql);
-        return sql;
-    });
-    prom.then((sql) => Database.query(sql));
-    prom.then((ret) => console.log(ret.toString()));
+    prom = prom.then(() => {
+            let sql = readScript("clean.sql");
+            console.log(sql);
+            return sql;
+        }).then((sql) => Database.query(sql))
+        .then((ret) => console.log(ret.toString()));
 }
 if (process.argv.includes("installDB")) {
-    prom.then(() => {
-        let sql = readScript("install.sql");
-        console.log(sql);
-        return sql;
-    });
-    prom.then((sql) => Database.query(sql));
-    prom.then((ret) => console.log(ret.toString()));
+    prom = prom.then(() => {
+            let sql = readScript("install.sql");
+            console.log(sql);
+            return sql;
+        }).then((sql) => Database.query(sql))
+        .then((ret) => console.log(ret.toString()));
 }
 if (process.argv.includes("demoDB")) {
-    prom.then(() => {
-        let sql = readScript("demo.sql");
-        console.log(sql);
-        return sql;
-    });
-    prom.then((sql) => Database.query(sql));
-    prom.then((ret) => console.log(ret.toString()));
+    prom = prom.then(() => {
+            let sql = readScript("demo.sql");
+            console.log(sql);
+            return sql;
+        }).then((sql) => Database.query(sql))
+        .then((ret) => console.log(ret.toString()));
 }
 prom.then(() => {
     console.log("\nAll Done!");
